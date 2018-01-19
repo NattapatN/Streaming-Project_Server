9284123103ac7fd2727a01745c24deee1b63f33f
Nattapat Niyomthai <34436384+NattapatN@users.noreply.github.com>
2018-01-19 22:28:59 +0700

    v 0.0.2

--- Server/src/Server_Main/ServerThread.java
+++ Server/src/Server_Main/ServerThread.java
@@ -5,6 +5,8 @@
  */
 package Server_Main;
 
+import Module.Download;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -18,6 +20,7 @@ import java.util.logging.Logger;
  */
 class ServerThread extends Thread{
     private Socket socket;
+    int buffersize;
     
     public ServerThread(Socket socket){
         this.socket = socket;
@@ -28,6 +31,21 @@ class ServerThread extends Thread{
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             int port = sSocket.getLocalPort();
             out.writeInt(port);
+            
+            Socket socket = sSocket.accept();
+            //read Meta
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            buffersize = in.readInt();
+            long size = in.readLong();
+            int length = in.readInt();
+            byte[] data = new byte[length];
+            in.readFully(data);
+            String name = new String(data,"UTF-8");
+            System.out.println("Name : "+name);
+            System.out.println("Size : "+size);
+            
+            Download down = new Download(socket,buffersize);
+            down.down(name,(int)size);
             
         } catch (IOException ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
